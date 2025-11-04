da4d9f07d5ae2d646a802a14f5f7491769397d69
Lorenzo <rueful.chest_0f@icloud.com>
2025-11-04 17:08:22 +0100

    3d adaptation and refactoring of samples, determinstic samples

--- main.py
+++ main.py
@@ -7,41 +7,59 @@ BBOX_STANDARD = []
 BBOX_SAMPLE = []
 
 
+def box_volume(box: list) -> float:
+    return (box[0][1] - box[0][0]) * (box[1][1] - box[1][0]) * (box[2][1] - box[2][0])
+
+
+def box_standard(r: float, R: float, origin: list[float] = [0.0, 0.0, 0.0]) -> list:
+    dy = r + origin[1]
+    dx = R + r + origin[0]
+    dz = r + origin[2]
+
+    return [[-dx, dx], [-dy, dy], [-dz, dz]]
+
+
+def box_sample(r: float, R: float, origin: list[float] = [0.0, 0.0, 0.0]) -> list:
+    dy = r
+    dx = R + r
+    dz = r
+
+    return [
+        [-dx + origin[0], dx + origin[1]],
+        [-dy + origin[1], dy + origin[1]],
+        [-dz + origin[2], dz + origin[2]],
+    ]
+
+
 def torus(
-    x: float,
-    y: float,
-    z: float,
+    samples: np.ndarray,
     r: float,
     R: float,
     origin: list[float] = [0.0, 0.0, 0.0],
-) -> bool:
+) -> np.ndarray:
     """
     Returns true if the given points (x, y, z) is inside the torus defined by the
     major radius R and the minor radius r.
     """
-    result = (
-        (np.sqrt((x - origin[0]) ** 2 + (y - origin[1]) ** 2) - R) ** 2
-        + (z - origin[2]) ** 2
-        - r**2
-        <= 0
-    ) or (
-        (np.sqrt((x - origin[0]) ** 2 + (y - origin[1]) ** 2) + R) ** 2
-        + (z - origin[2]) ** 2
-        - r**2
-        <= 0
-    )
-    return result
+    x, y, z = samples[:, 0], samples[:, 1], samples[:, 2]
+    x0, y0, z0 = origin[0], origin[1], origin[2]
+    val = (np.sqrt((x - x0) ** 2 + (y - y0) ** 2) - R) ** 2 + (z - z0) ** 2 - r**2
+    mask = val <= 0
+    return mask
 
 
-def sphere(x: float, y: float, z: float, k: float) -> bool:
+def sphere(samples: np.ndarray, k: float) -> np.ndarray:
     """
     Returns true if the given points (x, y, z) is inside the sphere defined by the
     radius k.
     """
-    result = x ^ 2 + y ^ 2 + z ^ 2 - k ^ 2 <= 0
-    return result
+    x, y, z = samples[:, 0], samples[:, 1], samples[:, 2]
+    val = x**2 + y**2 + z**2 - k**2
+    mask = val <= 0
+    return mask
 
 
+# TODO: this ok ?
 def torus2D(x: float, y: float, r: float, R: float, origin: list = [0, 0]) -> bool:
     """
     Returns true if the given points (x, y, z) is inside the torus defined by the
@@ -53,56 +71,33 @@ def torus2D(x: float, y: float, r: float, R: float, origin: list = [0, 0]) -> bo
     return result
 
 
-def box_standard(r: float, R: float, origin: list[float] = [0.0, 0.0, 0.0]) -> list:
-    dy = r + origin[1]
-    dx = R + r + origin[0]
+def samples(
+    box: list[list[float]],
+    n: int = 100_000,
+):
+    min_x, min_y, min_z = box[0][0], box[1][0], box[2][0]
+    max_x, max_y, max_z = box[0][1], box[1][1], box[2][1]
 
-    return [[-dx, dx], [-dy, dy]]
-
-
-def box_sample(r: float, R: float, origin: list[float] = [0.0, 0.0, 0.0]) -> list:
-    dy = r
-    dx = R + r
-    dz = r
-
-    return [[-dx + origin[0], dx + origin[1]], [-dy + origin[1], dy + origin[1]], [dz]]
+    samples = np.random.uniform(
+        [min_x, min_y, min_z], [max_x, max_y, max_z], size=(n, 3)
+    )
+    return samples
 
 
-def box_volume(box: list) -> float:
-    return (box[0][1] - box[0][0]) * (box[1][1] - box[1][0]) * (box[2][2] - [2][0])
-
-
-def one_sample(k: float, r: float, R: float, box: list, total: int) -> int:
-    min_x = box[0][0]
-    max_x = box[0][1]
-    min_y = box[1][0]
-    max_y = box[1][1]
-    min_z = box[2][0]
-    max_z = box[2][2]
-
-    num_x = rand()
-    num_y = rand()
-    num_z = rand()
-
-    x = num_x * (max_x - min_x) + min_x
-    y = num_y * (max_y - min_y) + min_y
-    z = num_z * (max_z - min_z) + min_z
-
-    total += sphere(x, y, z, k) and torus(x, y, z, r, R)
-
-
-def monte_carlo_2d(k: float, r: float, R: float, n: int = 100_000) -> float:
+def monte_carlo_3d(k: float, r: float, R: float, n: int = 100_000) -> float:
     """
-    Returns fraction of surface area of intersection between sphere and torus
+    Estimates volume of intersection between sphere (radius k) and torus (R, r).
     """
-    total = 0
-    box = box_standard(k, r, R)
-    mc_samples = []
-    mc_in_mask = []
-    for i in range(n):
-        total = one_sample(k, r, R, box, total)
+    box = box_standard(r, R)  # or box_sample(r, R)
 
-    return total * box_volume(box) / n
+    pts = samples(box, n)  # or deterministic_sequence(box, n)
+
+    inside_sphere = sphere(pts, k)
+    inside_torus = torus(pts, r, R)
+
+    count = np.sum(inside_sphere & inside_torus)
+
+    return count / n * box_volume(box)
 
 
 def mixed_sampling(
@@ -120,7 +115,7 @@ def mixed_sampling(
 
     # second box
     total2 = 0
-    box2 = box_sample(r, R, origin=origin)
+    box2 = box_sample(r, R, origin=[0.0, 0.0, 0.1])
 
     for _ in range(n):
         rnd = rand()
@@ -145,11 +140,48 @@ def surface_to_volume(R: float, surface_area: float) -> float:
     return volume
 
 
-def deterministic_sequence(seed: float) -> float:
+# TODO: to change with ndarray
+def find_centroid_2d(mc_samples: list, mc_in_mask: list) -> list:
     """
-    Generates a deterministic sequence of numbers based on the seed.
+    Takes all Monte Carlo sample points and the in-mask of those points to find the centroid of the surface area slice
     """
-    return 0
+
+    # mc_in_mask contains both sides of the intersection
+    # use only points with positive x
+
+    in_samples = np.array(
+        [
+            mc_samples[i]
+            for i in range(len(mc_samples))
+            if mc_in_mask[i][0] and mc_in_mask[i][1] and mc_samples[i][0] >= 0
+        ]
+    )
+
+    # the centroid is the mean of all points
+    centroid_x = np.mean(in_samples[:, 0])
+    centroid_y = np.mean(in_samples[:, 1])
+
+    return [centroid_x, centroid_y]
+
+
+# TODO: bottle neck
+def deterministic_sequence(points: np.ndarray, n: int = 100_000) -> np.ndarray:
+    """
+    Generates a deterministic sequence of numbers
+    based on the seed. Must be between 0 and 1.
+    """
+    x_0, y_0, z_0 = points[1], points[1], points[2]
+
+    samples = np.random.uniform(0, 0, size=(n, 3))
+    for i in range(n):
+        x_0 = (x_0 * 3.8 * (1 - x_0)) % 1
+        y_0 = (y_0 * 3.8 * (1 - y_0)) % 1
+        z_0 = (z_0 * 3.8 * (1 - z_0)) % 1
+        samples[i, 0] = x_0
+        samples[i, 1] = y_0
+        samples[i, 2] = z_0
+
+    return samples
 
 
 def _fill_between2D(
@@ -345,30 +377,15 @@ def plot_2d(
 
 
 if __name__ == "__main__":
-    print("Bombaclat!")
+    # Parameters
+    seed_deterministic = np.random.rand(3)
+    k = 1.0  # sphere radius
+    r = 0.5  # torus minor radius
+    R = 0.5  # torus major radius
+    n = 100_000  # Monte Carlo samples
 
-    # case a:
-    k = 1
-    R = 0.75
-    r = 0.4
-
-    area, mc_samples, mc_in_mask = monte_carlo_2d(k, r, R, n=10000)
-    volume = surface_to_volume(R, area)
-    volume_toro = 2 * np.pi * R * np.pi * r**2
-
-    print(f"areozza = {area} e volumozzo = {volume}, invece che {volume_toro}")
-
-    area = mixed_sampling(k, r, R, p=0.5, n=10000, origin=[0, 0.1])
-    volume = surface_to_volume(R, area)
-
-    print(f"areozza = {area} e volumozzo = {volume}, invece che {volume_toro}")
-
-    ax_sample = plot_2d(
-        k,
-        r,
-        R,
-        samples=mc_samples,
-        mc_in_mask=mc_in_mask,
-        box=box_sample(k, r, R, origin=[0, 0]),
-        save_path="img/2d.png",
-    )
+    sequence = deterministic_sequence(seed_deterministic, n)
+    print(sequence)
+    # --- Monte Carlo volume estimation ---
+    estimated_volume = monte_carlo_3d(k, r, R, n)
+    print(f"Estimated intersection volume (sphere ∩ torus): {estimated_volume:.4f}")
