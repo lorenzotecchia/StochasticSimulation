d985b5b77fe2b4a54fbdcf25b920fb5046b0dc53
philkre <p.kreiter@icloud.com>
2025-11-07 17:37:33 +0100

    q3 a2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from itertools import combinations, product
-
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
+from itertools import combinations, product
+from mpl_toolkits.axes_grid1 import host_subplot
 from matplotlib.axes import Axes
+<<<<<<< Updated upstream
 from numba import njit
 from scipy.stats import norm
+=======
+>>>>>>> Stashed changes
 from tqdm import tqdm
 
 
@@ -164,8 +168,14 @@ def monte_carlo_3d(
     """
     box = box_standard(r, R, origin=origin)  # or box_sample(r, R)
     if deterministic:
+<<<<<<< Updated upstream
         box = np.array(box, dtype=np.float64)
         pts = deterministic_sequence(np.random.uniform(0, 1, 3), box)
+=======
+        pts = deterministic_sequence(np.mean(box, axis=1), n)
+        print(box)
+        print(pts)
+>>>>>>> Stashed changes
     else:
         pts = samples(box, n)  # deterministic_sequence(box, n)
 
@@ -939,16 +949,19 @@ if __name__ == "__main__":
     R_b = 0.5
     n = 100_000  # Monte Carlo samples
     origin_shift = [0.0, 0.0, 0.1]
-    n_runs = 1000
+    n_runs = 10
 
     Q1 = False
+    Q2 = True
+    Q3 = False
+    PSWEEP = False
 
     # Question 1: volume of intersection between sphere and torus.
     if Q1:
         # case a:
         volumes_a = run_multi_executions(monte_carlo_3d, n_runs - 1, k, r_a, R_a)
         estimated_volume_a_3d, (pts, pts_in) = monte_carlo_3d(k, r_a, R_a, n)
-        volumes_a.append(estimated_volume_a_3d)
+        volumes_a = np.append(volumes_a, estimated_volume_a_3d)
         avg_volume_a = np.mean(volumes_a)
         std_volume_a = np.std(volumes_a)
 
@@ -958,7 +971,7 @@ if __name__ == "__main__":
         # case b:
         volumes_b = run_multi_executions(monte_carlo_3d, n_runs - 1, k, r_b, R_b)
         estimated_volume_b_3d, (pts, pts_in) = monte_carlo_3d(k, r_b, R_b, n)
-        volumes_b.append(estimated_volume_b_3d)
+        volumes_b = np.append(volumes_b, estimated_volume_b_3d)
         avg_volume_b = np.mean(volumes_b)
         std_volume_b = np.std(volumes_b)
 
@@ -968,7 +981,7 @@ if __name__ == "__main__":
         # case a but with 2d MC approximation
         volumes_a_2d = run_multi_executions(monte_carlo_2d, n_runs - 1, k, r_a, R_a)
         estimated_volume_a_2d, (pts_2d, pts_in_2d) = monte_carlo_2d(k, r_a, R_a, n)
-        volumes_a_2d.append(estimated_volume_a_2d)
+        volumes_a_2d = np.append(volumes_a_2d, estimated_volume_a_2d)
         avg_volume_a_2d = np.mean(volumes_a_2d)
         std_volume_a_2d = np.std(volumes_a_2d)
 
@@ -984,7 +997,7 @@ if __name__ == "__main__":
         # case b but with 2d MC approximation
         volumbes_b_2d = run_multi_executions(monte_carlo_2d, n_runs - 1, k, r_b, R_b)
         estimated_volume_b_2d, (pts_2d, pts_in_2d) = monte_carlo_2d(k, r_b, R_b, n)
-        volumbes_b_2d.append(estimated_volume_b_2d)
+        volumbes_b_2d = np.append(volumbes_b_2d, estimated_volume_b_2d)
         avg_volume_b_2d = np.mean(volumbes_b_2d)
         std_volume_b_2d = np.std(volumbes_b_2d)
 
@@ -1018,57 +1031,140 @@ if __name__ == "__main__":
         )
 
     # Question 2: change in estimate and error for determinstic sequence
+    if Q2:
+        volumes_deterministic_a = run_multi_executions(
+            monte_carlo_3d, n_runs - 1, k, r_a, R_a, deterministic=True
+        )
+        estimated_volume_deterministic_a, (pts, pts_in) = monte_carlo_3d(
+            k, r_a, R_a, n, deterministic=True
+        )
+        volumes_deterministic_a = np.append(
+            volumes_deterministic_a, estimated_volume_deterministic_a
+        )
+
+        print(volumes_deterministic_a)
+        avg_volume_deterministic_a = np.mean(volumes_deterministic_a)
+        std_volume_deterministic_a = np.std(volumes_deterministic_a)
+
+        plot_2d(
+            k, r_a, R_a, pts=pts, pts_in=pts_in, save_path="img/deterministic_a_2d.png"
+        )
+        plot_3d(
+            k, r_a, R_a, pts=pts, pts_in=pts_in, save_path="img/deterministic_a_3d.png"
+        )
+        print(
+            f"Q2, Deterministic Case A || Mean: {avg_volume_deterministic_a:.9f} || Standard Deviation: {std_volume_deterministic_a:.9f}"
+        )
+
+        volumes_deterministic_b = run_multi_executions(
+            monte_carlo_3d, n_runs - 1, k, r_b, R_b, deterministic=True
+        )
+        estimated_volume_deterministic_b, (pts, pts_in) = monte_carlo_3d(
+            k, r_b, R_b, n, deterministic=True
+        )
+        volumes_deterministic_b = np.append(
+            volumes_deterministic_b, estimated_volume_deterministic_b
+        )
+        avg_volume_deterministic_b = np.mean(volumes_deterministic_b)
+        std_volume_deterministic_b = np.std(volumes_deterministic_b)
+
+        plot_2d(
+            k, r_b, R_b, pts=pts, pts_in=pts_in, save_path="img/deterministic_b_2d.png"
+        )
+        plot_3d(
+            k, r_b, R_b, pts=pts, pts_in=pts_in, save_path="img/deterministic_b_3d.png"
+        )
+        print(
+            f"Q2, Deterministic Case B || Mean: {avg_volume_deterministic_b:.9f} || Standard Deviation: {std_volume_deterministic_b:.9f}"
+        )
 
     # Question 3: Off center torus
     # a) estimate volume
-    volumes_offcenter = []
-    volume_offcenter, (pts, pts_in) = monte_carlo_3d(
-        k, r_a, R_a, n, origin=origin_shift
-    )
-    volumes_offcenter.append(volume_offcenter)
-    for i in range(n_runs - 1):
-        volume_offcenter, _ = monte_carlo_3d(k, r_a, R_a, n, origin=origin_shift)
-        volumes_offcenter.append(volume_offcenter)
 
-    avg_volume_offcenter = np.mean(volumes_offcenter)
-    std_volume_offcenter = np.std(volumes_offcenter)
+    if Q3:
+        volumes_offcenter = np.empty(0)
+        volume_offcenter, (pts, pts_in) = monte_carlo_3d(
+            k, r_a, R_a, n, origin=origin_shift
+        )
+        volumes_offcenter = np.append(volumes_offcenter, volume_offcenter)
+        for i in range(n_runs - 1):
+            volume_offcenter, _ = monte_carlo_3d(k, r_a, R_a, n, origin=origin_shift)
+            volumes_offcenter = np.append(volumes_offcenter, volume_offcenter)
 
-    plot_2d(
-        k,
-        r_a,
-        R_a,
-        origin_t=origin_shift[-2:],
-        pts=pts,
-        pts_in=pts_in,
-        save_path="img/q3a_2d.png",
-    )
-    # plot_3d(k, r_a, R_a, pts=pts, pts_in=pts_in, save_path="img/q3a_3d.png")
-    print(
-        f"Q3, a) || Mean: {avg_volume_offcenter:.9f} || Standard Deviation: {std_volume_offcenter:.9f}"
-    )
-    # b) mixed sampling
-    p = 0.5
-    volumes_mixed = []
-    volume_mixed, ((pts1, pts1_in), (pts2, pts2_in)) = mixed_sampling(
-        k, r_a, R_a, n=n, p=p, origin=origin_shift
-    )
-    volumes_mixed.append(volume_mixed)
-    for i in range(n_runs - 1):
-        volume_mixed, _ = mixed_sampling(k, r_a, R_a, n=n, p=p, origin=origin_shift)
-        volumes_mixed.append(volume_mixed)
+        avg_volume_offcenter = np.mean(volumes_offcenter)
+        std_volume_offcenter = np.std(volumes_offcenter)
 
-    avg_volume_mixed = np.mean(volumes_mixed)
-    std_volume_mixed = np.std(volumes_mixed)
+        plot_2d(
+            k,
+            r_a,
+            R_a,
+            origin_t=origin_shift[-2:],
+            pts=pts,
+            pts_in=pts_in,
+            save_path="img/q3a_2d.png",
+        )
+        # plot_3d(k, r_a, R_a, pts=pts, pts_in=pts_in, save_path="img/q3a_3d.png")
+        print(
+            f"Q3, a) || Mean: {avg_volume_offcenter:.9f} || Standard Deviation: {std_volume_offcenter:.9f}"
+        )
+        # b) mixed sampling
+        p = 0.5
+        volumes_mixed = np.empty(0)
+        volume_mixed, ((pts1, pts1_in), (pts2, pts2_in)) = mixed_sampling(
+            k, r_a, R_a, n=n, p=p, origin=origin_shift
+        )
+        volumes_mixed = np.append(volumes_mixed, volume_mixed)
+        for i in range(n_runs - 1):
+            volume_mixed, _ = mixed_sampling(k, r_a, R_a, n=n, p=p, origin=origin_shift)
+            volumes_mixed = np.append(volumes_mixed, volume_mixed)
 
-    plot_mix_2d(
-        k,
-        r_a,
-        R_a,
-        origin_t=origin_shift,
-    )
-    print(
-        f"Q3, b) || Mean: {avg_volume_mixed:.9f} || Standard Deviation: {std_volume_mixed:.9f}"
-    )
+        avg_volume_mixed = np.mean(volumes_mixed)
+        std_volume_mixed = np.std(volumes_mixed)
+
+        plot_mix_2d(
+            k,
+            r_a,
+            R_a,
+            origin_t=origin_shift,
+        )
+        print(
+            f"Q3, b) || Mean: {avg_volume_mixed:.9f} || Standard Deviation: {std_volume_mixed:.9f}"
+        )
+
+    if PSWEEP:
+        # p-sweep
+        p_range = np.linspace(0.00001, 0.999, 100)
+        volumes_p_sweep = np.empty([len(p_range), n_runs])
+        for i, p in enumerate(tqdm(p_range, desc="Running p-sweep")):
+            for j in range(n_runs):
+                volume_p, _ = mixed_sampling(k, r_a, R_a, n=n, p=p, origin=origin_shift)
+                volumes_p_sweep[i, j] = volume_p
+
+        # plot p-sweep
+        ax1 = host_subplot(111)
+        ax2 = ax1.twinx()
+        mean = np.mean(volumes_p_sweep, axis=1)
+        std = np.std(volumes_p_sweep, axis=1)
+
+        ax1.plot(p_range, mean, color="green", lw=1, label="Mean")
+        # on second y axis, plot relative std dev
+        ax2.plot(
+            p_range,
+            std / mean,
+            color="blue",
+            lw=1,
+            label="Std Dev",
+        )
+
+        ax1.set_xlabel(r"Mixing Probability $p$")
+        ax1.set_ylabel("Estimated Volume Mean")
+        ax2.set_ylabel("Estimated Volume Std Dev")
+
+        ax1.set_title("Effect of Mixing Probability on Volume Estimate")
+        ax1.legend()
+        plt.tight_layout()
+        plt.savefig("img/p_sweep.png", dpi=300)
+        plt.close()
 
     # --- Generate results table ---
     names_methods = ["Mixed Sampling", "2D MC", "3D MC", "Deterministic Sequence"]
