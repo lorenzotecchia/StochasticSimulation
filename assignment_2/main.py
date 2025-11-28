479cec3c6d6b22c7c1039c68c31ee60be6e18de9
Lorenzo <rueful.chest_0f@icloud.com>
2025-11-28 14:15:32 +0100

    some new changes, minor ones

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -22,7 +22,7 @@ plt.rcParams.update(
         "ytick.labelsize": 10,
         "legend.fontsize": 10,
         "figure.figsize": (10, 6),
-        "lines.linewidth": 2,
+        "lines.linewidth": 1,
     }
 )
 
@@ -277,48 +277,6 @@ def check_validity(
     return t_stat < t_value
 
 
-def plot_waiting_times_cumavg(
-    waiting_times_collector: list,
-    reps_to_plot: int,
-    warm_up: int = 0,
-    save_path: str = "assignment_2/img/",
-    show: bool = False,
-):
-    plt.figure(figsize=(10, 6), dpi=300)
-    for r in range(reps_to_plot):
-        cumavg = np.cumsum(waiting_times_collector[r]) / np.arange(
-            1, len(waiting_times_collector[r]) + 1
-        )
-        plt.plot(cumavg, alpha=0.45, linewidth=1)
-
-    avg_per_customer = np.mean(waiting_times_collector, axis=0)
-    cumavg_per_customer = np.cumsum(avg_per_customer) / np.arange(
-        1, len(avg_per_customer) + 1
-    )
-    plt.plot(
-        cumavg_per_customer,
-        color="black",
-        label="ensemble-averaged cumulative mean",
-        linewidth=2.2,
-    )
-
-    if warm_up:
-        plt.axvline(x=warm_up, color="red", label="warm up", ls="--", linewidth=1.6)
-
-    plt.xlabel("Customer Index", fontsize=12)
-    plt.ylabel("Cumulative Average Waiting Time (min)", fontsize=12)
-    plt.grid(alpha=0.35, linestyle="--")
-    plt.tight_layout(pad=2.0)
-    plt.legend(fontsize=10, loc="best")
-
-    if save_path:
-        plt.savefig(save_path + "plot_bello.png", dpi=300, bbox_inches="tight")
-
-    if show:
-        plt.show()
-    plt.close()
-
-
 def std_sweep(
     arrival_rate: float,
     std_values: float,
@@ -384,8 +342,8 @@ def plot_queue_length_mean(queue_lengths_collector, scenario_name="Scenario"):
 
     interpolated = []
     for ql in queue_lengths_collector:
-        times = np.array([t for t, q in ql])
-        lengths = np.array([q for t, q in ql])
+        times = np.array([t for t, _ in ql])
+        lengths = np.array([q for _, q in ql])
         interpolated.append(np.interp(common_times, times, lengths))
 
     interpolated = np.array(interpolated)
@@ -745,6 +703,24 @@ def run_Q2B(df, plot_std_sweep=False):
         show=False,
         num_replications=R,
         colors=["blue", "orange", "green"],
+    )
+
+    plot_waiting_times_cumavg(
+        waiting_times_collector,
+        reps_to_plot=20,
+        warm_up=0,
+        save_path="assignment_2/img/",
+        show=False,
+    )
+
+    # Standard deviation sweep with CI bands
+    plot_std_sweep2(
+        arrival_rate,
+        [0.05, 1.0],
+        num_servers=1,
+        n_steps=100,
+        num_replications=40,
+        save_path="assignment_2/img/CI_sweep.png",
     )
 
     if plot_std_sweep:
